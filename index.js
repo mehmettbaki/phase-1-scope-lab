0b6d1953e730bd32c39c47a29ad0ee5132c6a779
memo <mehmetbaki.123@gmail.com>
2022-04-17 23:21:19 +0300

    m

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName='bob'
+
+
+function upperCaseCustomerName() {
+    
+    console.log(customerName.toUpperCase())
+}
+
+
+function setBestCustomer() {
+    var bestCustomer ='not bob'
+
+
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer="maybe bob"
+    
+
+}
+
+
+const leastFavoriteCustomer="memo"
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer="mrmo"
+}
